3cde04432397ef923ccaa572aa8dbed6c999e58d
maxirosanda <maxi_rosanda@hotmail.com>
2022-03-31 19:31:41 -0300

    clase

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let variable1 = prompt("Ingrese algo")
+/*
+let variable1 = prompt("Ingrese usuario")
 let resultado
 do{
     switch(variable1){
@@ -17,19 +18,57 @@ do{
 
     }
     console.log(resultado)
-    variable1 = prompt("ingrese otro valor")
+    variable1 = prompt("ingrese nombre de un usuario registrado")
 
 }while(variable1 != "esc")
+*/
 
-if(variable1 == "ana"){
-    console.log("hola na")
-}else if(variable1 == "jose"){
-    console.log("hola jose")
-}else if(variable1 == "maxi"){
-    console.log("hola maxi")
-}else{
-    console.log("hola")
+variable2 = 12
+
+// function suma(p1,p2){
+//     let resultado = p1 + p2
+//     return resultado
+// }
+
+// function resta(val1,val2){
+//     let resultado = val1 - val2
+//     return resultado 
+// }
+
+//  let resultado = 12 + resta(12,45)
+// console.log(resultado)
+
+function calculadora(par1,par2,operacion){
+    let resultado
+    switch (operacion){
+        case "suma":
+            resultado = par1 + par2
+            break
+        case "resta":
+            resultado = par1 - par2
+            break
+        case "multi":
+            resultado = par1 * par2
+            break
+        case "divi":
+            resultado = par1 / par2
+            break
+        case "cuadrado":
+            resultado = par1 * par1
+            break
+        default:
+            resultado= "error"
+    }
+    return resultado
+       
 }
+
+console.log(calculadora(12,12,"suma"))
+console.log(calculadora(3,12,"cuadrado"))
+console.log(calculadora(12,56,"resta"))
+
+
+
 
 
 
